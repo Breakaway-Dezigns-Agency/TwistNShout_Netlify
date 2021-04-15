f34b793449250a539795a93c3c40389a0d1a0ace
BrianG <bgarcia@mdg.agency>
2021-04-15 15:41:39 -0700

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
     `gatsby-plugin-sass`,
     // `gatsby-plugin-react-helmet`,
     // `gatsby-plugin-image`,
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     // `gatsby-plugin-gatsby-cloud`,
   ],
 };
