098835c7ce676f72e933ff4fb433aaba45275bd3
BrianG <bgarcia@mdg.agency>
2021-04-15 16:17:30 -0700

    index test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,22 @@ import {graphql, useStaticQuery} from 'gatsby'
 
 // markup
 const IndexPage = () => {
-  // const data = useStaticQuery(graphql`
-  //   query Index{
+  const data = useStaticQuery(graphql`
+    query Index {
+      index: wpgraphql {
+        page(id: "cG9zdDo4") {
+          uri
+          title
+          content
+        }
+      }
+    }
+  `)
 
-  //   }
-  // `)
+  const {content} = data.index.page
   return (
     <main>
-
+      <div dangerouslySetInnerHTML={{__html: content}} />
     </main>
   )
 }
